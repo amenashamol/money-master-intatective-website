bca17567f282101682997e1b989585e91030446c
amena <amena.ruet@gmail.com>
2022-02-17 23:48:32 +0600

    final code

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+//function for calculation
 
 function afterExpenseBalance(check){
     const totalIncome=document.getElementById('income-amount')
     const foodExpense=document.getElementById('food-cost')
     const rentExpense=document.getElementById('rent-cost')
     const clothExpense=document.getElementById('cloth-cost')
-  
-  
-    if (
+    const savingInput=document.getElementById('save-input')
+    
+    
+  if (
         totalIncome.value==""||
         totalIncome.value < 0 ||
         foodExpense.value == "" ||
@@ -14,49 +16,69 @@ function afterExpenseBalance(check){
         rentExpense.value < 0 ||
         rentExpense.value == "" ||
         clothExpense.value == ""||
-        clothExpense.value <0
+        clothExpense.value <0 
+       
   
-      ) {
-        
-        alert(" Input value is negative or emty")
+    ) {
+        let moreThan =document.getElementById('more-than') 
+        let cheakingValue=document.getElementById('input-value')
+        let lessThan =document.getElementById('less-than')
+
+        cheakingValue.style.display='block'
+        lessThan.style.display='none'
+        moreThan.style.display='none'
         return
-      }
+     }
+      
   
     
     const totalExpense=parseFloat(foodExpense.value)+parseFloat(rentExpense.value)+parseFloat(clothExpense.value)
     const  Balance = parseFloat(totalIncome.value)- totalExpense
-    document.getElementById('total-expense').innerText = totalExpense
+    
 
     if(check==true){
+        document.getElementById('total-expense').innerText = totalExpense
         if(Balance>totalExpense){
             document.getElementById('after-balance').innerText = Balance
         }
     
         else{
-            alert("Income amount is  less than total Expense amount")
-       }
+            const moreThan =document.getElementById('more-than') 
+            const cheakingValue=document.getElementById('input-value')
+            const lessThan =document.getElementById('less-than')
+
+            lessThan.style.display='block'
+            cheakingValue.style.display='none'
+            moreThan.style.display='none'
+        }
     }
 
     
     if(check==false) {
+        
         return Balance
+        
+        
     }   
     
-        
 }
   
  
+//calculate button click
 
 document.getElementById("calculate-button").addEventListener('click',function(){
       afterExpenseBalance(true)
       
     })
 
-  
+
+  //saving button click
+
 document.getElementById("save-button").addEventListener('click',function(){
+    
     let Balance = afterExpenseBalance(false)
   
-        
+      
     const totalIncome=document.getElementById('income-amount')
     const savingInput=document.getElementById('save-input')
   
@@ -70,7 +92,14 @@ document.getElementById("save-button").addEventListener('click',function(){
     }
     
     else{
-        return alert("Saving amount is  more than balance amounr")
+        const moreThan =document.getElementById('more-than') 
+        const cheakingValue=document.getElementById('input-value')
+        const lessThan =document.getElementById('less-than')
+
+          moreThan.style.display='block'
+          cheakingValue.style.display='none'
+          lessThan.style.display='none'
+         
     }
     
         
